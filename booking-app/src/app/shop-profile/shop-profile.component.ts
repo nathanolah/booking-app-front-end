1dd61d5390cb147adf8d64822077840269cda616
unknown <nolah@myseneca.ca>
2021-03-20 15:02:30 -0400

    update

--- booking-app/src/app/shop-profile/shop-profile.component.ts
+++ booking-app/src/app/shop-profile/shop-profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, interval, Subscription } from 'rxjs'; 
+
 import { BarbershopService } from '../Services/barbershop.service';
 import { BarberShop } from '../models/BarberShop';
 import { Barber } from '../models/Barber';
@@ -12,6 +14,7 @@ import { Customer } from '../models/Customer';
   styleUrls: ['./shop-profile.component.css']
 })
 export class ShopProfileComponent implements OnInit {
+  clicked: string = "";
 
   barberShop: BarberShop | undefined;
   querySub: any;
@@ -25,16 +28,22 @@ export class ShopProfileComponent implements OnInit {
   queueLastName: string = "";
   queueEmail: string = "";
 
-  listFirstName: string = "";
-  listLastName: string = "";
-  listEmail: string = "";
+  page: number = 1;
 
+
+
+
+  // listFirstName: string = "";
+  // listLastName: string = "";
+  // listEmail: string = "";
+
+  private updateSubscription: Subscription | undefined;
+  
   constructor(private data: BarbershopService, private route: ActivatedRoute, private router: Router) { }
-
+  
   ngOnInit(): void {
 
     this.querySub = this.route.params.subscribe(params => {
-
       // Barber Shop data
       this.data.getBarberShopById(params['id']).subscribe(data => {
         this.barberShop = data;
@@ -46,38 +55,75 @@ export class ShopProfileComponent implements OnInit {
       })
 
       // Waiting list
-      this.data.getQueueList(params['id']).subscribe(data => {
-          this.queueList = data;
+      // this.data.getQueueList(params['id']).subscribe(data => {
+      //     this.queueList = data;
+      // });
+
+      // Auto refresh page
+      this.updateSubscription = interval(1200).subscribe(() => {
+        this.updateQueueList(params['id'], +params['page'] || 1);
       });
       
     });
 
   }
 
-  // create the form module
-  // add the person to the list in queue (barberShopqueue)
-  // remove a person from the list
+  // Updates list after auto refresh
+  updateQueueList(id: string, num: any): any {
+      this.data.getQueueList(id, num).subscribe(data => {
+        this.queueList = data;
+        this.page = num;
+      });
+  }
+
+
+  public onSubmit(): void {
+    if (this.clicked === "enterQueue") {
+      this.newCustomer.firstName = this.queueFirstName;
+      this.newCustomer.lastName = this.queueLastName;
+      this.newCustomer.email = this.queueEmail;
   
-  enterQueue(): void {
-    // get the data from the form and console log it
-    this.newCustomer.firstName = this.listFirstName;
-
-    this.querySub = this.route.params.subscribe(params => {
-      this.data.addToQueue(params['id'] , this.newCustomer).subscribe(() => {
-        this.router.navigate([`/shop-profile/${params['id']}`])
+      console.log("testing: " + this.newCustomer.firstName + " "  + this.newCustomer.lastName + " " + this.newCustomer.email )
+  
+      this.querySub = this.route.params.subscribe(params => {
+        this.data.addToQueue(params['id'] , this.newCustomer).subscribe(() => {
+          this.router.navigate([`/shop-profile/${params['id']}`])
+        });
       });
-    });
-      
+        
+      this.queueFirstName = "";
+      this.queueLastName = "";
+      this.queueEmail = "";
 
-    
+      this.clicked = "";
+    }
 
+    if (this.clicked === "leaveQueue") {
 
-    // add user to queue
-    // this.querySub = this.route.params.subscribe(params => {
-    //   this.data.addToQueue(params['id']);
-    // });
+      this.newCustomer.firstName = this.queueFirstName;
+      this.newCustomer.lastName = this.queueLastName;
+      this.newCustomer.email = this.queueEmail;
+
+      this.querySub = this.route.params.subscribe(params => {
+        this.data.removeFromQueue(params['id'] , this.newCustomer).subscribe(() => {
+          this.router.navigate([`/shop-profile/${params['id']}`])
+        });
+      });
+
+      this.queueFirstName = "";
+      this.queueLastName = "";
+      this.queueEmail = ""
+
+      this.clicked = "";
+    }
 
   }
+
+  public onEnterClick() { this.clicked = "enterQueue"; } 
+  public onLeaveClick() { this.clicked = "leaveQueue"; }
+
+  // remove person from list
+  // pagination on list
 
   // ngOnDestroy() {
   //   if (this.querySub) {
