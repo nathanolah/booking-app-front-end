0eeb58f614526b5830ab1adfa498d7ca92e6d684
Sangmin-kim94 <63369672+Sangmin-kim94@users.noreply.github.com>
2021-03-22 13:35:32 -0400

    Add files via upload

--- booking-app/src/app/app.module.ts
+++ booking-app/src/app/app.module.ts
@@ -1,62 +1,79 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-
-
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import { SignupComponent } from './signup/signup.component';
 import { HeaderComponent } from './header/header.component';
+import { BarberListComponent } from './barber-list/barber-list.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
-import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { ShopProfileComponent } from './shop-profile/shop-profile.component';
-import { ReviewsListComponent } from './reviews-list/reviews-list.component';
+import { NewReviewComponent } from './new-review/new-review.component';
 import { AppointmentFormComponent } from './appointment-form/appointment-form.component';
-import { ReviewComponent } from './review/review.component';
-import { PagingComponent } from './paging/paging.component';
-import { CustomersComponent } from './customers/customers.component';
-import { CustomersListComponent } from './customers-list/customers-list.component';
-import { AppointmentListComponent } from './appointment-list/appointment-list.component';
-import { AppointmentByBarberComponent } from './appointment-by-barber/appointment-by-barber.component';
 import { BarberProfileComponent } from './barber-profile/barber-profile.component';
-import { AppointmentConfirmationComponent } from './appointment-confirmation/appointment-confirmation.component';
+import { ReviewListComponent } from './review-list/review-list.component';
+import { AboutComponent } from './about/about.component';
+import { FormsModule } from "@angular/forms";
+import { JwtModule } from "@auth0/angular-jwt";
+
+import { InterceptTokenService } from './intercept-token.service';
+import { LoginBarberComponent } from './login-barber/login-barber.component';
+import { AddBarberComponent } from './add-barber/add-barber.component';
+import { EditScheduleComponent } from './edit-schedule/edit-schedule.component';
+
+
+
+export function tokenGetter() {
+  return localStorage.getItem('access_token');
+}
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    BookComponent,
+    SignupComponent,
     HeaderComponent,
+    BarberListComponent,
     FooterComponent,
     HomeComponent,
-    PageNotFoundComponent,
     LoginComponent,
     RegisterComponent,
+    PageNotFoundComponent,
     ProfilePageComponent,
-    AccountSettingsComponent,
     ShopProfileComponent,
-    ReviewsListComponent,
+    NewReviewComponent,
     AppointmentFormComponent,
-    ReviewComponent,
-    PagingComponent,
-    CustomersComponent,
-    CustomersListComponent,
-    AppointmentListComponent,
-    AppointmentByBarberComponent,
     BarberProfileComponent,
-    AppointmentConfirmationComponent
+    ReviewListComponent,
+    AboutComponent,
+    LoginBarberComponent,
+    AddBarberComponent,
+    EditScheduleComponent
   ],
-  imports: [    
+  imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    FormsModule
+    FormsModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        authScheme: 'JWT'
+      }
+    })
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass:InterceptTokenService,
+    multi:true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
