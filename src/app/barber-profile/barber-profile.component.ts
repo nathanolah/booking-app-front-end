07233f209bc4862920fcd694a2a19fd9c65330c0
Sangmin-kim94 <63369672+Sangmin-kim94@users.noreply.github.com>
2021-04-21 22:05:04 -0400

    Update barber-profile.component.ts

--- src/app/barber-profile/barber-profile.component.ts
+++ src/app/barber-profile/barber-profile.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { Barber } from '../Barber';
+import { BarberShop } from '../BarberShop';
+import { DOCUMENT } from '@angular/common';
+
 import { BookingServiceService } from '../booking-service.service';
 
 @Component({
@@ -11,9 +14,11 @@ import { BookingServiceService } from '../booking-service.service';
 })
 export class BarberProfileComponent implements OnInit {
   isManager:boolean;
+  public shop: BarberShop;
+
   public barber:Barber;
   public token:any;
-  constructor(private book:BookingServiceService,private auth:AuthService, private route:Router, private aroute:ActivatedRoute) { }
+  constructor(@Inject(DOCUMENT) private doc:Document, private book:BookingServiceService,private auth:AuthService, private route:Router, private aroute:ActivatedRoute) { }
 
   ngOnInit(): void {
     let id=this.aroute.snapshot.params['id'];
@@ -21,13 +26,17 @@ export class BarberProfileComponent implements OnInit {
       
       this.barber=data;
 
-      console.log(this.barber);
+      
     })
     this.token=this.auth.readToken();
-    if(this.token.role=="Manager" || this.token.role =="Admin")
-    {
-      this.isManager=true;
-    }
+    let shop=this.aroute.snapshot.params['shop'];
+    this.book.getBarberShop(shop).subscribe(data=>{
+      if(this.token.role=="Manager" && (this.token._id == data.manager))
+      {
+        this.isManager=true;
+      }
+      this.shop =data;
+    })
   }
   clicked(e,id)
   {
@@ -45,5 +54,10 @@ export class BarberProfileComponent implements OnInit {
     this.book.deleteSchedule(id).subscribe(res=>{
       alert(res);
     })
+  }
+  
+  newSchedule(e,shop,id)
+  {
+    this.route.navigate(['/addSchedule', shop,id])
   }
 }
